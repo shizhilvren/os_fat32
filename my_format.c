bbf62e5d35392001df7355d8d86adebc2ee9cd7f
unknown <shizhilvren@foxmail.com>
2023-05-14 20:31:14 +0800

    修复strect部分位没有置0，在win下引起乱码错误。感谢Cocty的帮助。

--- my_format.c
+++ my_format.c
@@ -1,8 +1,9 @@
-#include"fs.h"
-#include"tool.h"
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include "fs.h"
+#include "tool.h"
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 /* 存在随机根目录dir乱码 原因不明 */
 /*
     接受一个参数 不得小于256MB 不得大于2097152MB(2TB)
@@ -10,189 +11,240 @@
 */
 #define MIN 256
 #define MAX 2097152
-//单位字节
-void vhdset(HD_FTRp vhd,u64 size){
-    strcpy(vhd->cookie,"conectix");
-    vhd->features=BigtoLittle32(0x00000002);
-    vhd->ff_version=BigtoLittle32(0x00010000);
-    vhd->data_offset=BigtoLittle64(0xFFFFFFFFFFFFFFFF);
-    vhd->timestamp=0x00000000;
-    strcpy(vhd->crtr_app,"myfs");
-    vhd->crtr_ver=BigtoLittle32(0x00060001);
+// 单位字节
+void vhdset(HD_FTRp vhd, u64 size)
+{
+    strcpy(vhd->cookie, "conectix");
+    vhd->features = BigtoLittle32(0x00000002);
+    vhd->ff_version = BigtoLittle32(0x00010000);
+    vhd->data_offset = BigtoLittle64(0xFFFFFFFFFFFFFFFF);
+    vhd->timestamp = 0x00000000;
+    strcpy(vhd->crtr_app, "myfs");
+    vhd->crtr_ver = BigtoLittle32(0x00060001);
     // strcpy((char*)vhd->crtr_os,"Wi2k");
-    vhd->crtr_os=BigtoLittle32(0x5769326b);
-    vhd->orig_size=BigtoLittle64(size);
-    vhd->curr_size=BigtoLittle64(size);
-    vhd->geometry=BigtoLittle32(0x03eb0c11);
-    vhd->type=BigtoLittle32(2); //类型
-    vhd->checksum=0;
+    vhd->crtr_os = BigtoLittle32(0x5769326b);
+    vhd->orig_size = BigtoLittle64(size);
+    vhd->curr_size = BigtoLittle64(size);
+    vhd->geometry = BigtoLittle32(0x03eb0c11);
+    vhd->type = BigtoLittle32(2); // 类型
+    vhd->checksum = 0;
     // strcmp(vhd->uuid,"                ");
-    memset(vhd->uuid,0xff,sizeof(vhd->uuid));
-    memset(vhd->reserved,0,sizeof(vhd->reserved));
-    u32 chksum=0;
-    u8* p=(u8*)vhd;
-    for(int i=0;i<512;i++){
-        chksum+=p[i];
+    memset(vhd->uuid, 0xff, sizeof(vhd->uuid));
+    memset(vhd->reserved, 0, sizeof(vhd->reserved));
+    u32 chksum = 0;
+    u8* p = (u8*)vhd;
+    for (int i = 0; i < 512; i++) {
+        chksum += p[i];
     }
-    vhd->checksum=BigtoLittle32(~chksum);
+    vhd->checksum = BigtoLittle32(~chksum);
 }
-//size为字节数
-void MBRset(MBRp mbr,int size){
-    mbr->sign=0x12345678;
-    (mbr->mbr_in[0]).flag=0;
-    (mbr->mbr_in[0]).FSflag=0x0b;
-    mbr->mbr_in[0].strart_chan=0x00000008;
-    mbr->mbr_in[0].all=size/BLOCKSIZE-mbr->mbr_in[0].strart_chan;
-    mbr->end=0xAA55;
+// size为字节数
+void MBRset(MBRp mbr, int size)
+{
+    mbr->sign = 0x12345678;
+    (mbr->mbr_in[0]).flag = 0;
+    (mbr->mbr_in[0]).FSflag = 0x0b;
+    mbr->mbr_in[0].strart_chan = 0x00000008;
+    mbr->mbr_in[0].all = size / BLOCKSIZE - mbr->mbr_in[0].strart_chan;
+    mbr->end = 0xAA55;
 }
-void FSInfoset(FSInfop fsi){
-    memset(fsi,0,BLOCKSIZE);
-    fsi->FSI_LeadSig=0x41615252;
-    fsi->FSI_StrucSig=0x61417272;
-    fsi->FSI_Free_Count=0xff;
-    fsi->FSI_Nxt_free=0xFFFFFFFF;
-    fsi->end=0xAA550000;
+void FSInfoset(FSInfop fsi)
+{
+    memset(fsi, 0, BLOCKSIZE);
+    fsi->FSI_LeadSig = 0x41615252;
+    fsi->FSI_StrucSig = 0x61417272;
+    fsi->FSI_Free_Count = 0xff;
+    fsi->FSI_Nxt_free = 0xFFFFFFFF;
+    fsi->end = 0xAA550000;
 }
 
-//size 为字节数
-void BS_BPSset(BS_BPBp bs_bpb,int size,int hiden){
-    my_strcpy(bs_bpb->BS_jmpBoot,"\xEB\x58\x90",sizeof(bs_bpb->BS_jmpBoot));
-    my_strcpy(bs_bpb->BS_OEMName,"MSDOS5.0",sizeof(bs_bpb->BS_OEMName));
-    bs_bpb->BPB_BytsPerSec=512;
-    bs_bpb->BPB_SecPerClus=8;//
-    bs_bpb->BPB_RsvdSecCnt=32;
-    bs_bpb->BPB_NumFATs=2;
-    bs_bpb->BPB_RootEntCnt=0;
-    bs_bpb->BPB_TotSec16=0;
-    bs_bpb->BPB_Media=0xf8;
-    bs_bpb->BPB_FATSz16=0;
-    bs_bpb->BPB_SecPerTrk=0x3f;
-    bs_bpb->BPB_NumHeads=0xff;
-    bs_bpb->BPB_HiddSec=hiden;
-    bs_bpb->BPB_TotSec32=size/BLOCKSIZE;
+// size 为字节数
+void BS_BPSset(BS_BPBp bs_bpb, int size, int hiden)
+{
+    my_strcpy(bs_bpb->BS_jmpBoot, "\xEB\x58\x90", sizeof(bs_bpb->BS_jmpBoot));
+    my_strcpy(bs_bpb->BS_OEMName, "MSDOS5.0", sizeof(bs_bpb->BS_OEMName));
+    bs_bpb->BPB_BytsPerSec = 512;
+    bs_bpb->BPB_SecPerClus = 8; //
+    bs_bpb->BPB_RsvdSecCnt = 32;
+    bs_bpb->BPB_NumFATs = 2;
+    bs_bpb->BPB_RootEntCnt = 0;
+    bs_bpb->BPB_TotSec16 = 0;
+    bs_bpb->BPB_Media = 0xf8;
+    bs_bpb->BPB_FATSz16 = 0;
+    bs_bpb->BPB_SecPerTrk = 0x3f;
+    bs_bpb->BPB_NumHeads = 0xff;
+    bs_bpb->BPB_HiddSec = hiden;
+    bs_bpb->BPB_TotSec32 = size / BLOCKSIZE;
 
-    int x=size/SPCSIZE/(512/4);
-    bs_bpb->BPB_FATSz32=x+(8-x%8);//强制4k对齐
-    bs_bpb->BPB_ExtFlags=0;
-    bs_bpb->BPB_FSVer=0;
-    bs_bpb->BPB_RootClis=2;
-    bs_bpb->BPB_FSInfo=1;
-    bs_bpb->BPB_BkBootSec=6;
-    memset(bs_bpb->BPB_Reserved,0,sizeof(bs_bpb->BPB_Reserved));
-    bs_bpb->BS_DrvNum=0x80;
-    bs_bpb->BS_Reserved1=0;
-    bs_bpb->BS_BootSig=0x29;
+    int x = size / SPCSIZE / (512 / 4);
+    bs_bpb->BPB_FATSz32 = x + (8 - x % 8); // 强制4k对齐
+    bs_bpb->BPB_ExtFlags = 0;
+    bs_bpb->BPB_FSVer = 0;
+    bs_bpb->BPB_RootClis = 2;
+    bs_bpb->BPB_FSInfo = 1;
+    bs_bpb->BPB_BkBootSec = 6;
+    memset(bs_bpb->BPB_Reserved, 0, sizeof(bs_bpb->BPB_Reserved));
+    bs_bpb->BS_DrvNum = 0x80;
+    bs_bpb->BS_Reserved1 = 0;
+    bs_bpb->BS_BootSig = 0x29;
     // char BS_VolID[4];
-    my_strcpy(bs_bpb->BS_VolLab,"NO NAME    ",11);
-    my_strcpy(bs_bpb->BS_FilSysType,"FAT32    ",8);
-    bs_bpb->end=0xAA55;
+    my_strcpy(bs_bpb->BS_VolLab, "NO NAME    ", 11);
+    my_strcpy(bs_bpb->BS_FilSysType, "FAT32    ", 8);
+    bs_bpb->end = 0xAA55;
 }
 
-int my_format(const ARGP arg){
-    char fatName[8]="WLt    ";
-    char fileName[ARGLEN]="fs.vhd";
-    const char helpstr[]=
-"\
+int my_format(const ARGP arg)
+{
+    char fatName[8] = "WLt    ";
+    char fileName[ARGLEN] = "fs.vhd";
+    const char helpstr[] = "\
 功能         格式化文件系统\n\
 语法格式     format size [name [namefile]]\n\
 szie        磁盘大小 单位MB\n\
 name        卷标名  默认 WTL\n\
 namefile    虚拟磁盘文件路径（当前目录下开始） 默认 fs.vhd\n";
-    DEBUG("%d",sizeof(BS_BPB));
+    DEBUG("%d", sizeof(BS_BPB));
     BLOCK4K block4k;
-    FILE *fp=NULL;
-    switch(arg->len){
-        case 3:
-            my_strcpy(fileName,arg->argv[2],ARGLEN);
-        case 2:
-            my_strcpy(fatName,arg->argv[1],8);
-        case 1:
-            if(strcmp(arg->argv[0],"/?")==0 && arg->len==1){
-                printf(helpstr);
-                return SUCCESS;
-            }else{
-                break;
-            }
+    BLOCK block;
+    FILE* fp = NULL;
+    switch (arg->len) {
+    case 3:
+        my_strcpy(fileName, arg->argv[2], ARGLEN);
+    case 2:
+        my_strcpy(fatName, arg->argv[1], 8);
+    case 1:
+        if (strcmp(arg->argv[0], "/?") == 0 && arg->len == 1) {
+            printf(helpstr);
+            return SUCCESS;
+        } else {
             break;
-        default:
-            strcpy(error.msg,"参数数量错误\n\x00");
-            printf("参数数量错误\n");
-            return ERROR;
+        }
+        break;
+    default:
+        strcpy(error.msg, "参数数量错误\n\x00");
+        printf("参数数量错误\n");
+        return ERROR;
     }
     // if(arg->len>3){
     //     strcpy(error.msg,"参数数量错误\n\x00");
     //     printf("参数数量错误\n");
     //     return ERROR;
     // }
-    
-    DEBUG("%s %d",arg->argv[0],ctoi(arg->argv[0]));
-    int size=ctoi(arg->argv[0]);
-    if(size<MIN||size>MAX){
-        strcpy(error.msg,"磁盘容量量错误\n\x00");
+
+    DEBUG("%s %d", arg->argv[0], ctoi(arg->argv[0]));
+    int size = ctoi(arg->argv[0]);
+    if (size < MIN || size > MAX) {
+        strcpy(error.msg, "磁盘容量量错误\n\x00");
         printf("磁盘容量量错误\n");
         return ERROR;
     }
-    int cut=size*K*K/SPCSIZE;
-    DEBUG("%d %d\n",sizeof(BLOCK),sizeof(BLOCK4K));
-    fp=fopen(fileName,"wb");
-    if(fp==NULL){
-        strcpy(error.msg,"文件打开错误\n\x00");
+    int cut = size * K * K / SPCSIZE;
+    DEBUG("%d %d\n", sizeof(BLOCK), sizeof(BLOCK4K));
+    fp = fopen(fileName, "wb");
+    if (fp == NULL) {
+        strcpy(error.msg, "文件打开错误\n\x00");
         printf("文件打开量错误\n");
         return ERROR;
     }
 
-    //生成文件本体
-    memset(&block4k,0,SPCSIZE);
-    for(int i=0;i<cut;i++){
-        do_write_block4k(fp,&block4k,-1);
+    int offset = 0;
+    int num = 0;
+    // 生成文件本体
+    memset(&block4k, 0, SPCSIZE);
+    memset(&block, 0, BLOCKSIZE);
+    for (int i = 0; i < cut; i++) {
+        do_write_block4k(fp, &block4k, -1);
     }
-    //处理vhd格式
+    do_write_block(fp, (BLOCK*)&block, -1, 0); // vhd 附加块
+    DEBUG("4K块数量 %d\n", cut);
+    fclose(fp);
+    fp = fopen(fileName, "rb+");
+    // 处理vhd格式
     HD_FTR vhd;
-    vhdset(&vhd,size*K*K);
-    do_write_block(fp,(BLOCK*)&vhd,-1,0);
-    
-    //磁盘分区
+    do_read_block(fp, (BLOCK*)&vhd, cut, 0);
+    vhdset(&vhd, size * K * K);
+    do_write_block(fp, (BLOCK*)&vhd, cut, 0);
+
+    // 磁盘分区
     MBR mbr;
-    MBRset(&mbr,size*K*K);
-    do_write_block(fp,(BLOCK*)&mbr,0,0);
+    do_read_block(fp, (BLOCK*)&mbr, 0, 0);
+    MBRset(&mbr, size * K * K);
+    do_write_block(fp, (BLOCK*)&mbr, 0, 0);
 
-    //fatBPB
+    // fatBPB
+    offset = mbr.mbr_in[0].strart_chan * BLOCKSIZE / SPCSIZE;
+    num = (mbr.mbr_in[0].strart_chan * BLOCKSIZE % SPCSIZE) / BLOCKSIZE;
     BS_BPB bs_pbp;
+    do_read_block(fp, (BLOCK*)&bs_pbp, offset, num);
     BS_BPSset(&bs_pbp,
-        size*K*K-mbr.mbr_in[0].strart_chan*BLOCKSIZE,
+        size * K * K - mbr.mbr_in[0].strart_chan * BLOCKSIZE,
         mbr.mbr_in[0].strart_chan);
-    do_write_block(fp,(BLOCK*)&bs_pbp,mbr.mbr_in[0].strart_chan*BLOCKSIZE/SPCSIZE,(mbr.mbr_in[0].strart_chan*BLOCKSIZE%SPCSIZE)/BLOCKSIZE);
-    
-    //初始化根目录
-    int start=bs_pbp.BPB_FATSz32*bs_pbp.BPB_NumFATs+bs_pbp.BPB_RsvdSecCnt+mbr.mbr_in[0].strart_chan+(bs_pbp.BPB_RootClis-2)*bs_pbp.BPB_SecPerClus;
-    DEBUG("开始扇区 %d\n",start);
+    do_write_block(fp, (BLOCK*)&bs_pbp, offset, num);
 
-    BLOCK block;
-    FAT_DSp fatdsp=(FAT_DSp)&block;
-    memset(fatdsp,0,sizeof(FAT_DS));
-    my_strcpy(fatdsp->name,fatName,8);
-    my_strcpy(fatdsp->named,"   ",3);
-    fatdsp->DIR_Attr=ATTR_VOLUME_ID;
-    do_write_block(fp,&block,start/8,start%8);
+    // 初始化根目录
+    int start = bs_pbp.BPB_FATSz32 * bs_pbp.BPB_NumFATs + bs_pbp.BPB_RsvdSecCnt + mbr.mbr_in[0].strart_chan + (bs_pbp.BPB_RootClis - 2) * bs_pbp.BPB_SecPerClus;
+    DEBUG("开始扇区 %d\n", start);
+
+    offset = start / 8;
+    num = start % 8;
+    FAT_DSp fatdsp = (FAT_DSp)&block;
+    do_read_block(fp, &block, offset, num);
+    memset(fatdsp, 0, sizeof(FAT_DS));
+    my_strcpy(fatdsp->name, fatName, 8);
+    my_strcpy(fatdsp->named, "   ", 3);
+    fatdsp->DIR_Attr = ATTR_VOLUME_ID;
+    do_write_block(fp, &block, offset, num);
+
+    //         for (int i = 0; i < cut; i++) {
+    //     for (int j = 0; j < 8; j++) {
+    //         int readlean = do_read_block(fp, &block, i, j);
+    //         printf("read len %d\n",readlean);
+    //         char* chr = &block;
+    //         for (int k = 0; k < SPCSIZE; k++) {
+    //             if ((*chr)) {
+    //                 printf("%02X\n \"%c\"", i * SPCSIZE + j * BLOCKSIZE,*chr);
+    //             }
+    //             // assert(!(*chr));
+    //             chr++;
+    //         }
+    //     }
+    // }
 
     FSInfo fsi;
+    offset = mbr.mbr_in[0].strart_chan * BLOCKSIZE / SPCSIZE;
+    num = (mbr.mbr_in[0].strart_chan * BLOCKSIZE % SPCSIZE) / BLOCKSIZE + 1;
+    do_read_block(fp, (BLOCK*)&fsi, offset, num);
     FSInfoset(&fsi);
     // do_write_block(fp,(BLOCK*)&bs_pbp,mbr.mbr_in[0].strart_chan*BLOCKSIZE/SPCSIZE,(mbr.mbr_in[0].strart_chan*BLOCKSIZE%SPCSIZE)/BLOCKSIZE);
-    do_write_block(fp,(BLOCK*)&fsi,mbr.mbr_in[0].strart_chan*BLOCKSIZE/SPCSIZE,(mbr.mbr_in[0].strart_chan*BLOCKSIZE%SPCSIZE)/BLOCKSIZE+1);
-    
-    //fat设置 (根目录初始化)
-    int str_fat1;
-    str_fat1=bs_pbp.BPB_RsvdSecCnt+mbr.mbr_in[0].strart_chan;
-    int str_fat2=bs_pbp.BPB_RsvdSecCnt+bs_pbp.BPB_FATSz32+mbr.mbr_in[0].strart_chan;
-    DEBUG("%d %d\n",str_fat1,str_fat2);
-    FAT fat;
-    fat.fat[0]=0x0ffffff8;
-    for(u32 i=1;i<=bs_pbp.BPB_RootClis;i++){
-        fat.fat[i]=FAT_END;
+    do_write_block(fp, (BLOCK*)&fsi, offset, num);
+
+    // fat设置 (根目录初始化)
+
+    const int str_fat1 = bs_pbp.BPB_RsvdSecCnt + mbr.mbr_in[0].strart_chan;
+    const int str_fat2 = bs_pbp.BPB_RsvdSecCnt + bs_pbp.BPB_FATSz32 + mbr.mbr_in[0].strart_chan;
+    const int str_fat_array[] = { str_fat1, str_fat2 };
+    DEBUG("%d %d\n", str_fat1, str_fat2);
+
+    for (int k = 0; k < 2; k++) {
+        FAT fat;
+        char* chr = &fat;
+        chr[0] = 1;
+        offset = str_fat_array[k] / 8;
+        num = str_fat_array[k] % 8;
+        do_read_block(fp, (BLOCK*)&fat, offset, num);
+        // memset(&fat, 0, BLOCKSIZE);
+        for (int i = 0; i < 512; i++) {
+            // printf("%x %02x %02x\n", &fat, i, *chr);
+            assert(!(*chr));
+            chr++;
+        }
+        fat.fat[0] = 0x0ffffff8;
+        for (u32 i = 1; i <= bs_pbp.BPB_RootClis; i++) {
+            fat.fat[i] = FAT_END;
+        }
+        // fat.fat[bs_pbp.BPB_RootEntCnt]=
+        do_write_block(fp, (BLOCK*)&fat, offset, num);
     }
-    // fat.fat[bs_pbp.BPB_RootEntCnt]=
-    do_write_block(fp,(BLOCK*)&fat,str_fat1/8,str_fat1%8);
-    do_write_block(fp,(BLOCK*)&fat,str_fat2/8,str_fat2%8);
 
     fclose(fp);
     DEBUG("磁盘申请成功\n");
--- tool.c
+++ tool.c
@@ -1,201 +1,220 @@
-#include"fs.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include "fs.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int do_write_block4k(FILE*fp,BLOCK4K* block4k,int offset){
-    if(offset==-1){
-        return fwrite(block4k,sizeof(BLOCK4K),1,fp);
-    }else{
-        fseek(fp,offset*SPCSIZE,SEEK_SET);
-        return fwrite(block4k,sizeof(BLOCK4K),1,fp);
+int do_write_block4k(FILE* fp, BLOCK4K* block4k, int offset)
+{
+    int ret = -1;
+    if (offset == -1) {
+        ret = fwrite(block4k, sizeof(BLOCK4K), 1, fp);
+    } else {
+        fseek(fp, offset * SPCSIZE, SEEK_SET);
+        ret = fwrite(block4k, sizeof(BLOCK4K), 1, fp);
     }
+    return ret;
 }
-int do_write_block(FILE*fp,BLOCK* block,int offset,int num){
-    if(offset==-1){
-        return fwrite(block,sizeof(BLOCK),1,fp);
-    }else{
-        fseek(fp,offset*SPCSIZE+num*BLOCKSIZE,SEEK_SET);
-        return fwrite(block,sizeof(BLOCK),1,fp);
+int do_write_block(FILE* fp, BLOCK* block, int offset, int num)
+{
+    int ret = -1;
+    if (offset == -1) {
+        ret = fwrite(block, sizeof(BLOCK), 1, fp);
+    } else {
+        // DEBUG("write %x-%x\n", offset * SPCSIZE + num * BLOCKSIZE, offset * SPCSIZE + num * BLOCKSIZE + sizeof(BLOCK));
+        fseek(fp, offset * SPCSIZE + num * BLOCKSIZE, SEEK_SET);
+        ret = fwrite(block, sizeof(BLOCK), 1, fp);
     }
-}
-
-int do_read_block4k(FILE*fp,BLOCK4K* block4k,int offset){
-    if(offset==-1){
-        return fread(block4k,sizeof(BLOCK4K),1,fp);
-    }else{
-        fseek(fp,offset*SPCSIZE,SEEK_SET);
-        return fread(block4k,sizeof(BLOCK4K),1,fp);
-    }
-}
-int do_read_block(FILE*fp,BLOCK* block,int offset,int num){
-    if(offset==-1){
-        return fread(block,sizeof(BLOCK),1,fp);
-    }else{
-        fseek(fp,offset*SPCSIZE+num*BLOCKSIZE,SEEK_SET);
-        return fread(block,sizeof(BLOCK),1,fp);
-    }
+    return ret;
 }
 
-u32 L2R(FileSystemInfop fsip,u32 num){
+int do_read_block4k(FILE* fp, BLOCK4K* block4k, int offset)
+{
+
+    int ret = -1;
+    if (offset == -1) {
+        ret = fread(block4k, sizeof(BLOCK4K), 1, fp);
+    } else {
+        fseek(fp, offset * SPCSIZE, SEEK_SET);
+        ret = fread(block4k, sizeof(BLOCK4K), 1, fp);
+    }
+    return ret;
+}
+int do_read_block(FILE* fp, BLOCK* block, int offset, int num)
+{
+    
+    int ret = -1;
+    if (offset == -1) {
+        ret = fread(block, sizeof(BLOCK), 1, fp);
+    } else {
+        DEBUG("read %x-%x\n", offset * SPCSIZE + num * BLOCKSIZE, offset * SPCSIZE + num * BLOCKSIZE + sizeof(BLOCK));
+        fseek(fp, offset * SPCSIZE + num * BLOCKSIZE, SEEK_SET);
+        ret = fread(block, sizeof(BLOCK), 1, fp);
+    }
+    return ret;
+}
+
+u32 L2R(FileSystemInfop fsip, u32 num)
+{
     // DEBUG("lj %u->%u\n",num,(fsip->MBR_start+
-            // fsip->BPB_RsvdSecCnt+
-            // fsip->BPB_FATSz32*fsip->BPB_NumFATs)/fsip->BPB_SecPerClus+num-2);
-    return (fsip->MBR_start+
-            fsip->BPB_RsvdSecCnt+
-            fsip->BPB_FATSz32*fsip->BPB_NumFATs)/fsip->BPB_SecPerClus+num-2;
+    // fsip->BPB_RsvdSecCnt+
+    // fsip->BPB_FATSz32*fsip->BPB_NumFATs)/fsip->BPB_SecPerClus+num-2);
+    return (fsip->MBR_start + fsip->BPB_RsvdSecCnt + fsip->BPB_FATSz32 * fsip->BPB_NumFATs) / fsip->BPB_SecPerClus + num - 2;
 }
 
-u32 getNext(FileSystemInfop fsip,u32 num){
-    if(num/(512/4)>fsip->BPB_FATSz32){
+u32 getNext(FileSystemInfop fsip, u32 num)
+{
+    if (num / (512 / 4) > fsip->BPB_FATSz32) {
         return 0;
     }
-    u32 cuNum=num/(512/4);
-    u32 index=num%(512/4);
+    u32 cuNum = num / (512 / 4);
+    u32 index = num % (512 / 4);
     FAT fat;
-    do_read_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[0]+cuNum)/8,(fsip->FAT[0]+cuNum)%8);
+    do_read_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[0] + cuNum) / 8, (fsip->FAT[0] + cuNum) % 8);
     return fat.fat[index];
 }
 
-int newfree(FileSystemInfop fsip,u32 num){
+int newfree(FileSystemInfop fsip, u32 num)
+{
     FAT fat;
-    u32 cuNum=num/(512/4);
-    u32 index=num%(512/4);
-    for(u32 i=0;i<fsip->BPB_FATSz32;i++){
-        do_read_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[0]+i)/8,(fsip->FAT[0]+i)%8);
-        for(int j=0;j<512/4;j++){
-            if(fat.fat[j]==FAT_FREE){
-                fat.fat[j]=FAT_END;
-                for(int i=0;i<fsip->BPB_NumFATs;i++){
-                    do_write_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[i]+i)/8,(fsip->FAT[i]+i)%8);
+    u32 cuNum = num / (512 / 4);
+    u32 index = num % (512 / 4);
+    for (u32 i = 0; i < fsip->BPB_FATSz32; i++) {
+        do_read_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[0] + i) / 8, (fsip->FAT[0] + i) % 8);
+        for (int j = 0; j < 512 / 4; j++) {
+            if (fat.fat[j] == FAT_FREE) {
+                fat.fat[j] = FAT_END;
+                for (int i = 0; i < fsip->BPB_NumFATs; i++) {
+                    do_write_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[i] + i) / 8, (fsip->FAT[i] + i) % 8);
                 }
-                if(num!=0){
-                    do_read_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[0]+cuNum)/8,(fsip->FAT[0]+cuNum)%8);
-                    fat.fat[index]=i*(512/4)+j;
-                    for(int i=0;i<fsip->BPB_NumFATs;i++){
-                        do_write_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[i]+cuNum)/8,(fsip->FAT[i]+cuNum)%8);
+                if (num != 0) {
+                    do_read_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[0] + cuNum) / 8, (fsip->FAT[0] + cuNum) % 8);
+                    fat.fat[index] = i * (512 / 4) + j;
+                    for (int i = 0; i < fsip->BPB_NumFATs; i++) {
+                        do_write_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[i] + cuNum) / 8, (fsip->FAT[i] + cuNum) % 8);
                     }
                 }
                 BLOCK4K block4k;
-                memset(&block4k,0,SPCSIZE);
-                do_write_block4k(fsip->fp,&block4k,L2R(fsip,i*(512/4)+j));
-                return i*(512/4)+j;
+                memset(&block4k, 0, SPCSIZE);
+                do_write_block4k(fsip->fp, &block4k, L2R(fsip, i * (512 / 4) + j));
+                return i * (512 / 4) + j;
             }
         }
     }
     return 0;
 }
-int delfree(FileSystemInfop fsip,u32 num){
+int delfree(FileSystemInfop fsip, u32 num)
+{
     FAT fat;
-    u32 cuNum=num/(512/4);
-    u32 index=num%(512/4);
-    do_read_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[0]+cuNum)/8,(fsip->FAT[0]+cuNum)%8);
-    u32 ret=fat.fat[index];
-    fat.fat[index]=FAT_FREE;
-    for(int i=0;i<fsip->BPB_NumFATs;i++){
-        do_write_block(fsip->fp,(BLOCK*)&fat,(fsip->FAT[i]+cuNum)/8,(fsip->FAT[i]+cuNum)%8);
+    u32 cuNum = num / (512 / 4);
+    u32 index = num % (512 / 4);
+    do_read_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[0] + cuNum) / 8, (fsip->FAT[0] + cuNum) % 8);
+    u32 ret = fat.fat[index];
+    fat.fat[index] = FAT_FREE;
+    for (int i = 0; i < fsip->BPB_NumFATs; i++) {
+        do_write_block(fsip->fp, (BLOCK*)&fat, (fsip->FAT[i] + cuNum) / 8, (fsip->FAT[i] + cuNum) % 8);
     }
     return ret;
 }
 
-char* my_strcpy(char *to,const char*from,int size ){
-    for(int i=0;i<size;i++){
-        to[i]=from[i];
+char* my_strcpy(char* to, const char* from, int size)
+{
+    for (int i = 0; i < size; i++) {
+        to[i] = from[i];
     }
     return to;
 }
 
 // 取得命令参数
-int getargv(ARGP argp){
-    char cmd[ARGLEN*10];
-    if(gets(cmd)==NULL){
-        argp->len=0;
+int getargv(ARGP argp)
+{
+    char cmd[ARGLEN * 10];
+    if (gets(cmd) == NULL) {
+        argp->len = 0;
         return SUCCESS;
     }
-    int num=0;
-    int len=0;
-    int flag=0;
-    for(u32 i=0;i<strlen(cmd);i++){
-        if(len>=ARGLEN){
+    int num = 0;
+    int len = 0;
+    int flag = 0;
+    for (u32 i = 0; i < strlen(cmd); i++) {
+        if (len >= ARGLEN) {
             return ERROR;
         }
-        if(num>=ARGNUM){
+        if (num >= ARGNUM) {
             return ERROR;
         }
-        if(cmd[i]==' ' ||cmd[i]=='\t' ||cmd[i]=='\r'){
-            flag=0;
+        if (cmd[i] == ' ' || cmd[i] == '\t' || cmd[i] == '\r') {
+            flag = 0;
             continue;
         }
-        if(flag==0){
+        if (flag == 0) {
             num++;
-            len=0;
+            len = 0;
         }
-        (argp->argv)[num-1][len]=cmd[i];
-        (argp->argv)[num-1][len+1]=0;
+        (argp->argv)[num - 1][len] = cmd[i];
+        (argp->argv)[num - 1][len + 1] = 0;
         len++;
-        flag=1;
+        flag = 1;
     }
-    argp->len=num;
+    argp->len = num;
     return SUCCESS;
 }
 
-
-//数字字符串转换为int类型数字,如果非法返回INF，不考虑溢出
-int ctoi(const char* ch){
-    int ret=0;
-    for(u32 i=0;i<strlen(ch);i++){
-        ret*=10;
-        if('0'<=ch[i]&&ch[i]<='9'){
-            ret+=ch[i]-'0';
-        }else{
+// 数字字符串转换为int类型数字,如果非法返回INF，不考虑溢出
+int ctoi(const char* ch)
+{
+    int ret = 0;
+    for (u32 i = 0; i < strlen(ch); i++) {
+        ret *= 10;
+        if ('0' <= ch[i] && ch[i] <= '9') {
+            ret += ch[i] - '0';
+        } else {
             return INF;
         }
     }
     return ret;
 }
 
-int nameCheckChange(const char name[ARGLEN],char name38[12]){
-    if(strlen(name)>12||strlen(name)<=0){
+int nameCheckChange(const char name[ARGLEN], char name38[12])
+{
+    if (strlen(name) > 12 || strlen(name) <= 0) {
         return ERROR;
     }
-    int point=-1;
-    for(u32 i=0;i<strlen(name);i++){
-        if(name[i]=='.'){
-            point=i;
+    int point = -1;
+    for (u32 i = 0; i < strlen(name); i++) {
+        if (name[i] == '.') {
+            point = i;
             continue;
         }
-        if(!(isalnum(name[i]) || isalpha(name[i]) || isspace(name[i]) ||
-                 name[i]=='$' || name[i]=='%' || name[i]=='\'' || name[i]=='-' ||
-                  name[i]=='_' || name[i]=='@' || name[i]=='~' || name[i]=='`' || 
-                  name[i]=='!' || name[i]=='(' || name[i]==')' || name[i]=='{' || 
-                  name[i]=='}' || name[i]=='^' || name[i]=='#' || name[i]=='&')){
+        if (!(isalnum(name[i]) || isalpha(name[i]) || isspace(name[i]) || name[i] == '$' || name[i] == '%' || name[i] == '\'' || name[i] == '-' || name[i] == '_' || name[i] == '@' || name[i] == '~' || name[i] == '`' || name[i] == '!' || name[i] == '(' || name[i] == ')' || name[i] == '{' || name[i] == '}' || name[i] == '^' || name[i] == '#' || name[i] == '&')) {
             return ERROR;
         }
     }
-    if( ((point!=-1) && (point<=8 && strlen(name)-point-1<=3 )) 
-                || (point==-1 && strlen(name)<=8 )){
-        memset(name38,' ',11);
-        name38[11]='\0';
-        if(point==0){
+    if (((point != -1) && (point <= 8 && strlen(name) - point - 1 <= 3))
+        || (point == -1 && strlen(name) <= 8)) {
+        memset(name38, ' ', 11);
+        name38[11] = '\0';
+        if (point == 0) {
             return ERROR;
-        }else if(point!=-1){
-            for(int i=0;i<point;i++){
-                name38[i]=name[i];
+        } else if (point != -1) {
+            for (int i = 0; i < point; i++) {
+                name38[i] = name[i];
             }
-            for(int i=point+1;i<(int)strlen(name);i++){
-                name38[i-point+8-1]=name[i];
+            for (int i = point + 1; i < (int)strlen(name); i++) {
+                name38[i - point + 8 - 1] = name[i];
             }
-        }else{
-            for(int i=0;i<(int)strlen(name);i++){
-                name38[i]=name[i];
+        } else {
+            for (int i = 0; i < (int)strlen(name); i++) {
+                name38[i] = name[i];
             }
         }
         return SUCCESS;
     }
-    return ERROR;;
+    return ERROR;
+    ;
 }
 
-int debug_in(char * format,...){
+int debug_in(char* format, ...)
+{
     return 0;
 }
